a534bc0e817e7970641f10ec63042d1ef5f80591
jeelidev <jeelidev@gmail.com>
2025-10-04 02:12:42 -0400

    first commit

---
+++ next.config.js
@@ -0,0 +1,26 @@
+import { withPayload } from '@payloadcms/next/withPayload'
+
+import redirects from './redirects.js'
+
+const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
+        const url = new URL(item)
+
+        return {
+          hostname: url.hostname,
+          protocol: url.protocol.replace(':', ''),
+          pathname: '/**',
+        }
+      }),
+    ],
+  },
+  reactStrictMode: true,
+  redirects,
+}
+
+export default withPayload(nextConfig, { devBundleServerPackages: false })
---
+++ src/environment.d.ts
@@ -0,0 +1,18 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      PAYLOAD_SECRET: string
+      DATABASE_URI: string
+      NEXT_PUBLIC_SERVER_URL: string
+      VERCEL_PROJECT_PRODUCTION_URL: string
+      S3_BUCKET: string
+      S3_ACCESS_KEY_ID: string
+      S3_SECRET_ACCESS_KEY: string
+      S3_ENDPOINT: string
+    }
+  }
+}
+
+// If this file has no import/export statements (i.e. is a script)
+// convert it into a module by adding an empty export statement.
+export {}
